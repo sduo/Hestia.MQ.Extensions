86627fc9f715502f7ff2d7948e42998d6cbfbb55
SDuo <sduo@vip.qq.com>
2023-04-17 16:14:16 +0800

    1.0.0

---
+++ src/MessageQueueExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Hestia.MQ.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Hestia.MQ
+{
+    public static class MessageQueueExtensions
+    {
+        public static IMessageQueue GetMessageQueue(this IServiceProvider services, string name, StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+        {
+            return services.GetServices<IMessageQueue>().FirstOrDefault(mq => string.Equals(mq.Name, name, comparison));
+        }
+
+    }
+}
